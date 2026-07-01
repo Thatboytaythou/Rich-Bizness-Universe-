3be82570eca9836b171b0b6bf5c55f0ec960fc0d
Thatboytaythou <tayben.h@gmail.com>
2026-07-01 00:22:25 -0400

    Load XP gauge styles from module

--- src/xp-gauge.js
+++ src/xp-gauge.js
@@ -1,5 +1,10 @@
 import { supabase } from './supabase-client.js';
 
+const css = document.createElement('link');
+css.rel = 'stylesheet';
+css.href = '/src/xp-gauge.css?v=xp-gauge-1';
+document.head.appendChild(css);
+
 const fmt = (n) => Number(n || 0).toLocaleString();
 
 function ensureGauge() {
