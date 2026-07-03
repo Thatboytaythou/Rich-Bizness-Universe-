a9eef6d53d925d0947d048dc409acca66a2001aa
Thatboytaythou <tayben.h@gmail.com>
2026-07-03 04:11:29 -0400

    Add image asset map

---
+++ src/rb-assets.js
@@ -0,0 +1,16 @@
+export const RB_ASSETS = {
+  logo3d: '/images/3D-logo.png.jpeg',
+  avatarHero: '/images/Avatar-hero-Banner.png.jpeg',
+  gtaStudio: '/images/GTA-STUDIO.png.jpeg',
+  arcadeCity: '/images/acarde-city.png.jpeg',
+  avatarCityBanner: '/images/avatar-city-banner.png.jpeg',
+  gamingHero: '/images/gaming-hero.png.jpeg',
+  gamingLogo: '/images/gaming-logo.png.jpeg',
+  gtaStyleElite: '/images/gta-style-elite.png.jpeg',
+  heroBanner: '/images/hero-banner.png',
+  heroLogo: '/images/hero-logo.png.jpeg',
+  musicLogo: '/images/music-log.png.jpeg',
+  sportsLogo: '/images/sports-logo.png.jpeg',
+  travelCity: '/images/travel-city.png.jpeg',
+  emojiSign: '/images/emoji-sign1.png'
+};
