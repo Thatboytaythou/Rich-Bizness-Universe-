f9830be37682ba694201ec9934fac88f31839058
Thatboytaythou <tayben.h@gmail.com>
2026-07-13 18:37:39 -0400

    Build Boss Walk Battle runtime

---
+++ games/boss-walk-battle/game.ts
@@ -0,0 +1,2 @@
+import { createArcadeGame } from '../shared/runtime/create-arcade-game';
+createArcadeGame({ slug:'boss-walk-battle', title:'Boss Walk Battle', instruction:'Hit the beat and own the runway.', actionLabel:'BOSS STEP', targetScore:1500, timeLimitSeconds:40, pointsPerAction:100 });
