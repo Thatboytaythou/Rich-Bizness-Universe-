e63fff8a67116645d5c62ae082cc9d888547eb24
Thatboytaythou <tayben.h@gmail.com>
2026-07-13 18:37:34 -0400

    Build Bizness Party Room runtime

---
+++ games/bizness-party-room/game.ts
@@ -0,0 +1,2 @@
+import { createArcadeGame } from '../shared/runtime/create-arcade-game';
+createArcadeGame({ slug:'bizness-party-room', title:'Bizness Party Room', instruction:'Build the party meter before time runs out.', actionLabel:'HYPE THE ROOM', targetScore:1200, timeLimitSeconds:45, pointsPerAction:75 });
