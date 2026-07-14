516945a1fb3ea76fdae962c5cf9673892f2655d3
Thatboytaythou <tayben.h@gmail.com>
2026-07-13 21:29:24 -0400

    Mount global realtime XP runtime

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -3,6 +3,8 @@ import './styles/base.css';
 import './styles/cinematic.css';
 import './styles/identity.css';
 import './styles/avatar-premium.css';
+import './styles/xp-runtime.css';
 import { bootstrap } from './bootstrap';
+import { mountXpRuntime } from './core/xp/xp-runtime';
 
-void bootstrap();
+void bootstrap().then(() => mountXpRuntime());
