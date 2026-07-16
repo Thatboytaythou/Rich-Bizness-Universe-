c837cb08f725907e0530b637aad3b301999d37f8
Thatboytaythou <tayben.h@gmail.com>
2026-07-16 03:46:14 -0400

    Add canonical config package root entry

---
+++ packages/config/src/index.ts
@@ -0,0 +1,8 @@
+export { ROUTES, safeInternalRoute } from './routes';
+export type { RouteKey } from './routes';
+export { TABLES } from './tables';
+export type { TableName } from './tables';
+export { BUCKETS } from './buckets';
+export type { BucketName } from './buckets';
+export { GAMES } from './games';
+export type { GameDefinition, GameSlug } from './games';
