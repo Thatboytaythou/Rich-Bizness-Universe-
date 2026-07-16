9bfc288ef449dc45b78998023dad042a16043c6e
Thatboytaythou <tayben.h@gmail.com>
2026-07-16 04:06:38 -0400

    Export canonical XP contracts

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -6,3 +6,13 @@ export { BUCKETS } from './buckets';
 export type { BucketName } from './buckets';
 export { GAMES } from './games';
 export type { GameDefinition, GameSlug } from './games';
+export {
+  XP_QUEUE_STATUS,
+  XP_TERMINAL_STATUS,
+  XP_SECTIONS,
+  XP_PER_LEVEL,
+  levelFromXp,
+  currentLevelXp,
+  nextLevelXp
+} from './xp';
+export type { XpQueueStatus, XpTerminalStatus, XpSection, XpSnapshot } from './xp';
