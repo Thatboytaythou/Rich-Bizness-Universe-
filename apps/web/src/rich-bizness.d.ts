876921afe6cede156f8a628e0a63899c8454e8c9
Thatboytaythou <tayben.h@gmail.com>
2026-09-01 01:46:35 -0400

    Declare required app-shell mount invariant for strict pages

--- apps/web/src/rich-bizness.d.ts
+++ apps/web/src/rich-bizness.d.ts
@@ -4,4 +4,8 @@ declare global {
   interface Window {
     __rbPageCleanup?: (() => void | Promise<void>) | null;
   }
+
+  interface Document {
+    querySelector<E extends Element = Element>(selectors: '#app'): E;
+  }
 }
