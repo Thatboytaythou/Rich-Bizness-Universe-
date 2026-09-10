bff2361c7b75b712375ca3d95be19b0d1a4a6acf
Thatboytaythou <tayben.h@gmail.com>
2026-09-10 19:41:39 -0400

    Preserve existing table contract while completing RPC contract

--- apps/web/src/core/supabase/database.types.ts
+++ apps/web/src/core/supabase/database.types.ts
@@ -5,8 +5,35 @@ type Table = { Row: Row; Insert: Row; Update: Row; Relationships: [] };
 
 export type Database = {
   public: {
-    Tables: { [table: string]: Table };
-    Views: { [view: string]: Table };
+    Tables: {
+      admin_audit_logs: Table; admin_roles: Table; api_jobs: Table; api_request_logs: Table; api_webhook_events: Table;
+      app_env_contract: Table; app_runtime_modules: Table; audio_listening_history: Table;
+      avatar_animation_clips: Table; avatar_character_presets: Table; avatar_controller_profiles: Table; avatar_inventory: Table; avatar_items: Table; avatar_models: Table; avatar_motion_state: Table;
+      background_presets: Table; badges: Table; brand_assets: Table;
+      content_review_queue: Table; creator_alert_subscriptions: Table; creator_available_balances: Table; creator_page_settings: Table;
+      dm_call_participants: Table; dm_call_sessions: Table; dm_message_attachments: Table; dm_message_reactions: Table; dm_message_reads: Table; dm_messages: Table; dm_thread_members: Table; dm_threads: Table; dm_typing_status: Table;
+      feature_flags: Table; feed_comments: Table; feed_post_likes: Table; feed_post_views: Table; feed_posts: Table; followers: Table;
+      game_alert_subscriptions: Table; game_categories: Table; game_challenges: Table; game_clips: Table; game_comments: Table; game_levels: Table; game_likes: Table; game_missions: Table; game_moves: Table; game_platform_accounts: Table; game_player_progress: Table; game_rewards: Table; game_room_members: Table; game_rooms: Table; game_runtime_manifests: Table; game_scores: Table; game_sessions: Table; game_stream_links: Table; game_tournaments: Table; gamer_profiles: Table; games: Table; gaming_uploads: Table;
+      layout_presets: Table;
+      live_alert_subscriptions: Table; live_categories: Table; live_chat_messages: Table; live_reactions: Table; live_recordings: Table; live_stream_bans: Table; live_stream_cards: Table; live_stream_members: Table; live_stream_purchases: Table; live_streams: Table; live_tips: Table; live_view_sessions: Table; livekit_room_events: Table;
+      meta_avatars: Table; meta_chat_messages: Table; meta_inventory: Table; meta_items: Table; meta_portals: Table; meta_room_members: Table; meta_rooms: Table; meta_stream_links: Table; meta_visits: Table; meta_world_likes: Table; meta_worlds: Table;
+      moderation_reports: Table;
+      music_comments: Table; music_likes: Table; music_play_events: Table; music_tracks: Table;
+      notification_groups: Table; notification_reads: Table;
+      platform_analytics_events: Table; platform_announcements: Table; playlist_tracks: Table; playlists: Table;
+      podcast_comments: Table; podcast_episodes: Table; podcast_likes: Table; podcast_shows: Table;
+      product_likes: Table; product_views: Table; products: Table;
+      profile_theme_settings: Table; profile_view_events: Table; profiles: Table; public_profile_cards: Table; push_devices: Table;
+      radio_likes: Table; radio_sessions: Table; radio_stations: Table; rank_rules: Table; rb_personality_settings: Table; rb_secret_rooms: Table; rich_notifications: Table; route_access_rules: Table; route_registry: Table;
+      search_clicks: Table; search_queries: Table; section_theme_settings: Table;
+      sports_alert_subscriptions: Table; sports_brackets: Table; sports_broadcasts: Table; sports_comments: Table; sports_leagues: Table; sports_pick_results: Table; sports_picks: Table; sports_posts: Table; sports_profiles: Table; sports_reactions: Table; sports_teams: Table; sports_uploads: Table;
+      storage_bucket_routes: Table; store_cart_items: Table; store_comments: Table; store_notifications: Table; store_orders: Table; store_seller_profiles: Table; stripe_sync_events: Table; system_health_checks: Table;
+      tournament_players: Table; tracks: Table; trust_events: Table;
+      upload_processing_queue: Table; uploads: Table; user_avatar_loadouts: Table; user_badges: Table; user_custom_screens: Table; user_levels: Table; user_product_unlocks: Table; user_sessions: Table; user_settings: Table; user_xp_ledger: Table;
+      vip_live_access: Table; watch_comments: Table; watch_likes: Table; watch_progress: Table; watchlist_items: Table;
+      xp_event_queue: Table; xp_events: Table; xp_rule_bindings: Table; xp_section_progress: Table;
+    };
+    Views: { active_brand_assets: Table; creator_products: Table };
     Functions: {
       rb_admin_action: { Args: { p_action: string; p_target_id: string; p_value?: Json }; Returns: Json };
       rb_admin_snapshot: { Args: { p_limit?: number }; Returns: Json };
